de42dd1f21f03e4783269a9e1db51439c7272355
byynaryy <email@gamil.com>
2017-07-11 16:58:37 +0200

    copy an object, extend an object and check for a property in an object

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,19 @@
-module.exports.copy = undefined;
+module.exports.copy = function(obj) {
+    let objct = {}
+    return Object.assign(objct, obj);
+}
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+    Object.assign(dest, src);
+    return dest;
+} 
 
-module.exports.hasElems = undefined;
+
+module.exports.hasElems = function(obj, arr) {
+    for(var x of arr) {
+        if(!obj.hasOwnProperty(x)) {
+            return false;
+        }
+    }
+    return true;
+}
