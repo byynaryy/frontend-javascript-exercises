f307d60cbfa25a784b5f65837cab4eb3947a36b1
byynaryy <email@gamil.com>
2017-07-11 14:44:39 +0200

    return an array from each of the keys or values of an oject

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -6,4 +6,11 @@ module.exports.getValues = function(obj) {
     return Object.values(obj);
 }
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(obj) {
+    var table = [];
+
+    for(var x in obj) {
+        table.push(x+" is "+obj[x]);
+    }
+    return table;
+} 
