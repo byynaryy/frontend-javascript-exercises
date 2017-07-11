c9ad02244bbe95c3b3d72a7c727e4e9545a5d9ce
Jeff <Home@Jeffs-MacBook-Pro.local>
2017-07-11 07:08:25 +0200

    dealing with loops

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,16 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arr) {
+    return arr.reduce(function(pv, cv) {return pv + cv;}, 0);
+}
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(str) {
+    return str.split(',').map(Function.prototype.call, String.prototype.toLowerCase);
+} 
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(arr) {
+    var table = [];
+
+    for(var i = 0; i < arr.length; i++) {
+        table[i] = i+" is "+arr[i];
+    }
+    return table;
+}
